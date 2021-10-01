25882fc3e1380e45e509a635a0cb79edf849e17c
limeu <limeu@DESKTOP-KFBSC5A>
2021-10-01 20:24:36 +0900

    해시/전화번호 목록/91.7

--- src/TelNumber_L.java
+++ src/TelNumber_L.java
@@ -1,24 +1,33 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class TelNumber_L {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] phone_book = { "12", "123", "1235", "567", "88" };
+		String[] phone_book = { "12", "23", "25", "5 67", "2 34" };
 		solution(phone_book);
 	}
 
 	public static boolean solution(String[] phone_book) {
 		boolean answer = true;
-		
-		for(int i =0; i<phone_book.length-1;i++) {
-			for(int j=i+1; j<phone_book.length;j++) {
-				if(phone_book[j].startsWith(phone_book[i])) {
+		ArrayList<String> change = new ArrayList<>();
+		for(int i =0; i<phone_book.length;i++) {
+			change.add(phone_book[i].replaceAll(" ", ""));
+		}
+		Collections.sort(change);
+		a :for(int i =0; i<change.size()-1;i++) {
+			for(int j=i+1; j<change.size();j++) {
+				if(!change.get(j).substring(0,1).equals(change.get(i).substring(0,1))) {
+					break;
+				}
+				if(change.get(j).startsWith(change.get(i))) {
 					answer=false;
+					break a;
 				}
 				
 			}
 		}
-
 		return answer;
 	}
 
