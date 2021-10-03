ad447371c1e664eb068e398d6e40abee50d7b8f5
limeu <limeu@DESKTOP-KFBSC5A>
2021-10-03 16:18:59 +0900

    스택,큐/프린터

--- src/MaxNumber.java
+++ src/MaxNumber.java
@@ -8,11 +8,11 @@ public class MaxNumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] numbers= {6,10,2};
+		int[] numbers= {40,40,4,3,30,31,3,3};
 		String a =solution(numbers);
 	}
 	public static String solution(int[] numbers) {
-		String answer = "";
+        String answer = "";
         ArrayList<String> strNumbers = new ArrayList<>();
 
         for(int i=0; i<numbers.length;i++) {
@@ -20,21 +20,23 @@ public class MaxNumber {
         }
         Collections.sort(strNumbers);
         Collections.reverse(strNumbers);
-        //System.out.println(strNumbers);
+       // System.out.println(strNumbers);
         for(int i= 0; i<strNumbers.size()-1;i++) {
         	if(strNumbers.get(i).substring(0, 1).equals(strNumbers.get(i+1).substring(0, 1))) {
-        		//System.out.println(strNumbers.get(i).substring(0, 1)+","+strNumbers.get(i+1).substring(0, 1));
+        		System.out.println(strNumbers.get(i).substring(0, 1)+","+strNumbers.get(i+1).substring(0, 1));
         		if(Integer.parseInt(strNumbers.get(i))%10<Integer.parseInt(strNumbers.get(i+1))%10) {
-        			//System.out.println(strNumbers);
+        			System.out.println(strNumbers);
         			String tmp = strNumbers.get(i);
         			strNumbers.set(i, strNumbers.get(i+1));
         			strNumbers.set(i+1, tmp);
-        			//System.out.println(strNumbers);
+        			System.out.println(strNumbers);
         		}
         	}
-        	answer=answer+strNumbers.get(i);       	
         }
-        answer=answer+strNumbers.get(strNumbers.size()-1);
+        for(int i= 0; i<strNumbers.size();i++) {
+        	answer=answer+strNumbers.get(i);
+        }
+        System.out.println(answer);
         
         return answer;
     }
