4ddef31c1842704127e71341a14611a6a3424d75
limeu <limeu@DESKTOP-KFBSC5A>
2021-10-04 21:16:06 +0900

    정렬/가장큰수/36.4

--- src/MaxNumber.java
+++ src/MaxNumber.java
@@ -8,37 +8,40 @@ public class MaxNumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] numbers= {40,40,4,3,30,31,3,3};
-		String a =solution(numbers);
+		int[] numbers = { 3,40,43, 30,31,30,32,33, 34, 5, 9,40,41,44,45,4,444,445,446};
+		System.out.println(solution(numbers));
 	}
-	public static String solution(int[] numbers) {
-        String answer = "";
-        ArrayList<String> strNumbers = new ArrayList<>();
 
-        for(int i=0; i<numbers.length;i++) {
-        	strNumbers.add(String.valueOf(numbers[i]));
-        }
-        Collections.sort(strNumbers);
-        Collections.reverse(strNumbers);
-       // System.out.println(strNumbers);
-        for(int i= 0; i<strNumbers.size()-1;i++) {
-        	if(strNumbers.get(i).substring(0, 1).equals(strNumbers.get(i+1).substring(0, 1))) {
-        		System.out.println(strNumbers.get(i).substring(0, 1)+","+strNumbers.get(i+1).substring(0, 1));
-        		if(Integer.parseInt(strNumbers.get(i))%10<Integer.parseInt(strNumbers.get(i+1))%10) {
-        			System.out.println(strNumbers);
-        			String tmp = strNumbers.get(i);
-        			strNumbers.set(i, strNumbers.get(i+1));
-        			strNumbers.set(i+1, tmp);
-        			System.out.println(strNumbers);
-        		}
-        	}
-        }
-        for(int i= 0; i<strNumbers.size();i++) {
-        	answer=answer+strNumbers.get(i);
-        }
-        System.out.println(answer);
-        
-        return answer;
-    }
+	public static String solution(int[] numbers) {
+		String answer = "";
+		ArrayList<String> strNumbers = new ArrayList<>();
+
+		for (int i = 0; i < numbers.length; i++) {
+			strNumbers.add(String.valueOf(numbers[i]));
+		}
+		Collections.sort(strNumbers);
+		Collections.reverse(strNumbers);
+		System.out.println(strNumbers);
+		for(int j = 0; j<strNumbers.size();j++) {
+			for(int i = 0;i<strNumbers.size()-1;i++) {
+				if(strNumbers.get(i).substring(0, 1).equals(strNumbers.get(i+1).substring(0, 1))) {
+					if(Integer.parseInt(strNumbers.get(i))%10<Integer.parseInt(strNumbers.get(i+1))%10) {
+						String max=strNumbers.get(i+1);
+						String min=strNumbers.get(i);	
+						strNumbers.set(i, max);
+						strNumbers.set(i+1, min);
+						System.out.println(strNumbers);
+						continue;
+					} else {
+						continue;
+					}
+				}
+			}
+		}
+		for (int i = 0; i < strNumbers.size(); i++) {
+			answer = answer + strNumbers.get(i);
+		}
+		return answer;
+	}
 
 }
