0d793250900c58ee9dd8412e5a4fc644eea2ecdd
limeu <limeu@DESKTOP-KFBSC5A>
2021-10-06 17:55:21 +0900

    정렬/가장큰수/54.5

--- src/MaxNumber.java
+++ src/MaxNumber.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,38 +9,32 @@ public class MaxNumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] numbers = { 3,40,43, 30,31,30,32,33, 34, 5, 9,40,41,44,45,4,444,445,446};
+		int[] numbers = { 5, 0, 10, 15, 20 };// 5,44,4,43,40,404
 		System.out.println(solution(numbers));
 	}
 
 	public static String solution(int[] numbers) {
 		String answer = "";
-		ArrayList<String> strNumbers = new ArrayList<>();
 
-		for (int i = 0; i < numbers.length; i++) {
-			strNumbers.add(String.valueOf(numbers[i]));
-		}
-		Collections.sort(strNumbers);
-		Collections.reverse(strNumbers);
-		System.out.println(strNumbers);
-		for(int j = 0; j<strNumbers.size();j++) {
-			for(int i = 0;i<strNumbers.size()-1;i++) {
-				if(strNumbers.get(i).substring(0, 1).equals(strNumbers.get(i+1).substring(0, 1))) {
-					if(Integer.parseInt(strNumbers.get(i))%10<Integer.parseInt(strNumbers.get(i+1))%10) {
-						String max=strNumbers.get(i+1);
-						String min=strNumbers.get(i);	
-						strNumbers.set(i, max);
-						strNumbers.set(i+1, min);
-						System.out.println(strNumbers);
-						continue;
-					} else {
-						continue;
-					}
+		for (int j = 0; j < numbers.length; j++) {
+			for (int i = 0; i < numbers.length - (1 + j); i++) {
+				String onetwo = String.valueOf(numbers[i]) + String.valueOf(numbers[i + 1]);
+				String twoone = String.valueOf(numbers[i + 1]) + String.valueOf(numbers[i]);
+				if (Integer.parseInt(onetwo) < Integer.parseInt(twoone)) {
+					int tmp = numbers[i];
+					numbers[i] = numbers[i + 1];
+					numbers[i + 1] = tmp;
 				}
+//				for(int n =0; n<numbers.length;n++) {
+//					System.out.print(numbers[n]+",");
+//				}
+//				System.out.println();
 			}
+			answer = String.valueOf(numbers[numbers.length - 1 - j]) + answer;
+//			System.out.println("==="+answer+"========");	
 		}
-		for (int i = 0; i < strNumbers.size(); i++) {
-			answer = answer + strNumbers.get(i);
+		if ("0".equals(answer.substring(0, 1))) {
+			answer = "0";
 		}
 		return answer;
 	}
