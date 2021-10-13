657f8b5c86ed88359cae270ae111d6f37a09a854
limeu <limeu@DESKTOP-KFBSC5A>
2021-10-13 22:01:28 +0900

    탐욕법(Greedy)/조이스틱/

--- src/Joystic.java
+++ src/Joystic.java
@@ -10,6 +10,9 @@ public class Joystic {
 	public static int solution(String name) {
 		int answer = 0;
 		String[] make = new String[name.length()];
+		int right=0;
+		int left=0;
+		int turn=name.length()-1;
 		// 초기값A 설정
 		for (int i = 0; i < name.length(); i++) {
 			make[i] = "A";
@@ -23,39 +26,30 @@ public class Joystic {
 				// 알파벳이 A에 더 가까우면 up버튼을 누른다.
 				answer = answer + name.charAt(i) - 'A';
 			}
-			// right
-			if (!(i == name.length() - 1)) {
-				answer++;
-			}
+		}	
+		int minMove = name.length() - 1;
+        for(int i = 0 ; i < name.length() ; i++) {
+            if(name.charAt(i) != 'A') {
+                int next = i+1;
 
+                while(next < name.length() && name.charAt(next) == 'A') {
+                    next++;
+                }
+                int move = 2 * i + name.length() - next;
+
+                minMove = Math.min(move, minMove);
+            }
+        }
+		return answer+minMove;
+	}
+
+	private static int min(int i, int j) {
+		// TODO Auto-generated method stub
+		if(i<j) {
+			return i;
+		} else {
+			return j;
 		}
-		
-		// left의 경우
-		int left=0;
-		int aCount=0;
-		int aa=name.charAt(1);
-		
-		//A가 1개인 경우
-		if(name.length()==3&&'A'==name.charAt(1)) {
-			return answer-1;
-		}
-		//A가2개 이상의 경우
-		for(int i = 0; i < name.length()-1; i++){
-			if('A'==name.charAt(i)&&'A'==name.charAt(i+1)) {
-				aCount++;
-				if(aa>i) {
-					aa=i;
-				}
-			}else {
-				break;
-			}
-		}
-		if(aCount>0) {
-			aa=aa-1;
-			left=aCount+1-aa;
-		}
-		
-		return answer-left;
 	}
 
 }
